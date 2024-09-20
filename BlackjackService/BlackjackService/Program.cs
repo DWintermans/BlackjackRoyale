5894b190131ef7027b327aeaf92603075890e6fa
DW <ps218325@gmail.com>
2024-09-20 14:00:12 +0200

    [ADD] placeholder to test CI

--- BlackjackService/BlackjackService/Program.cs
+++ BlackjackService/BlackjackService/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace BlackjackService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("WebSocket connection will be established here.");
+		}
+	}
+}
