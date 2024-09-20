b70d7f766e5288d0a494c5e4092b7506f7df4e35
DW <ps218325@gmail.com>
2024-09-20 13:39:56 +0200

    [ADD] ValidateUser (login) unittest

--- BlackjackAPI/BlackjackLogic/Interfaces/Repository/IAccountRepository.cs
+++ BlackjackAPI/BlackjackLogic/Interfaces/Repository/IAccountRepository.cs
@@ -2,7 +2,7 @@
 {
 	public interface IAccountRepository
 	{
-		(int user_id, string user_name, byte[] hashed_pw, byte[] salt) RetrieveLoginInformation(string UsernameOrEmail);
+		(int user_id, string user_name, byte[] hashed_pw, byte[] salt) RetrieveLoginInformation(string username);
 		(byte[] hashed_pw, byte[] salt) RetrieveSalt_HashInformation(int user_id);
 		int CreateAccount(string username, string hashed_password, string salt);
 		bool IsUsernameTaken(string username);
---
+++ BlackjackAPI/BlackjackTest/Account/ValidateUser.cs
@@ -0,0 +1,90 @@
+﻿using BlackjackLogic.Interfaces.Repository;
+
+namespace BlackjackTest.Account
+{
+	[TestClass]
+	public class AttemptLogin
+	{
+		private Mock<IAccountRepository> _mockAccountRepository;
+		private AccountLogic _accountLogic;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_mockAccountRepository = new Mock<IAccountRepository>();
+			_accountLogic = new AccountLogic(_mockAccountRepository.Object);
+		}
+
+		[TestMethod]
+		public void ValidateUser_ValidCredentials_Returns_UserID()
+		{
+			// Arrange
+			byte[] hashedPassword = Convert.FromBase64String("2WG9Qvv+Mg4OElDUfZQOBA==");
+			byte[] salt = Convert.FromBase64String("p/o/W2ZF/F0y9Om3pyjVew==");
+
+			var fakeDBLoginInfo = (
+				user_id: 1,
+				user_name: "testuser",
+				hashed_pw: hashedPassword,
+				salt: salt
+			);
+
+			//mock repo
+			_mockAccountRepository.Setup(r => r.RetrieveLoginInformation("testuser")).Returns(fakeDBLoginInfo);
+
+			// Act
+			int result = _accountLogic.ValidateUser("testuser", "password");
+
+			// Assert
+			Assert.AreEqual(1, result);
+		}
+
+		[TestMethod]
+		public void ValidateUser_InvalidPassword_Returns_0()
+		{
+			// Arrange
+			byte[] hashedPassword = Convert.FromBase64String("2WG9Qvv+Mg4OElDUfZQOBA==");
+			byte[] salt = Convert.FromBase64String("p/o/W2ZF/F0y9Om3pyjVew==");
+
+			var fakeDBLoginInfo = (
+				user_id: 1,
+				user_name: "testuser",
+				hashed_pw: hashedPassword,
+				salt: salt
+			);
+
+			//mock repo
+			_mockAccountRepository.Setup(r => r.RetrieveLoginInformation("testuser")).Returns(fakeDBLoginInfo);
+
+			// Act
+			int result = _accountLogic.ValidateUser("testuser", "PASSWORD");
+
+			// Assert
+			Assert.AreEqual(0, result);
+		}
+
+		[TestMethod]
+		public void ValidateUser_InvalidUsername_Returns_0()
+		{
+			// Arrange
+			byte[] hashedPassword = Convert.FromBase64String("2WG9Qvv+Mg4OElDUfZQOBA==");
+			byte[] salt = Convert.FromBase64String("p/o/W2ZF/F0y9Om3pyjVew==");
+
+			var fakeDBLoginInfo = (
+				user_id: 1,
+				user_name: "testuser",
+				hashed_pw: hashedPassword,
+				salt: salt
+			);
+
+			//mock repo
+			_mockAccountRepository.Setup(r => r.RetrieveLoginInformation("testuser")).Returns(fakeDBLoginInfo);
+
+			// Act
+			int result = _accountLogic.ValidateUser("Testuser", "password");
+
+			// Assert
+			Assert.AreEqual(0, result);
+		}
+	}
+}
